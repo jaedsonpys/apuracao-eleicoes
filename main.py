84bb31ac5c7160472c23f555a6dd3f261201242d
Jaedson Silva <contacanal798@gmail.com>
2022-10-30 16:18:15 -0300

    adding percentage_loadbar function

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import requests
 
 resource = 'https://resultados.tse.jus.br'
 url = f'{resource}/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json'
+
+
+def percentage_loadbar(percentage: float):
+    load_bar = ['='] * 100
+    candidate_bar = load_bar[:int(percentage)]
+
+    while len(candidate_bar) < 100:
+        candidate_bar.append(' ')
+
+    print(f'{percentage}% [{"".join(candidate_bar)}]')
